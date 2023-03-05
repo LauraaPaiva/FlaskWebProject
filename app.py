9263da238b6607231f9bb28d691400c67fc535cf
LauraaPaiva <lauraapaiva94@gmail.com>
2023-03-05 16:32:41 -0300

    Adicionar arquivos de projeto.

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+wsgi_app = app.wsgi_app
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/sobre')
+def contato():
+    return render_template('sobre.html')
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
