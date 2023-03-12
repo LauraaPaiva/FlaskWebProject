1fa3a0daae8e217366515e8094bb245a2f31894e
LauraaPaiva <lauraapaiva94@gmail.com>
2023-03-12 19:07:35 -0300

    Implementação do interface do formulário de login e criar conta

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, flash, redirect
+from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
 
 wsgi_app = app.wsgi_app
+
+app.config['SECRET_KEY'] = 'a19c1477bb0fc235da954711ecb04d45'
 
 @app.route('/')
 def home():
@@ -11,6 +14,19 @@ def home():
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    redirect(url_for('home'))
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 if __name__ == '__main__':
     import os
